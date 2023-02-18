e3710b11e5da6f63c200353ad77c8489526dfa22
pransandip <[pransandip@gmail.com]>
2023-02-18 16:16:53 +0530

    Feb18, 2023

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,11 @@ let count = 0;
 
 app.get("/", (req: Request, res: Response) => {
 	count += 1;
-	console.log(`
-	IP: ${req.ip} server hits: ${count} time.`);
+	console.log(`IP: ${req.ip} server hits: ${count} time.`);
 
 	res.status(200).json({ msg: "healthy" });
 });
 
 app.listen(PORT, () => {
-	console.log(`
-	TS: server is running on port ${PORT}.`);
+	console.log(`TS: server is running on port ${PORT}.`);
 });

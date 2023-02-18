791a8ffdb4f027dc1992dbc9099b15462d808bb2
pransandip <[pransandip@gmail.com]>
2023-02-18 16:09:25 +0530

    Lint stage husky added:Test

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const PORT = 8000;
+let count = 0;
+
+app.get("/", (req: Request, res: Response) => {
+	count += 1;
+	console.log(`
+	IP: ${req.ip} server hits: ${count} time.`);
+
+	res.status(200).json({ msg: "healthy" });
+});
+
+app.listen(PORT, () => {
+	console.log(`
+	TS: server is running on port ${PORT}.`);
+});
